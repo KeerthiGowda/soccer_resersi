5d4dd49267a8060331b273545761d85950c4948a
KeerthiGowda <keerthis@vt.edu>
2016-11-04 15:11:10 -0400

    Update soccer_reversi.js
    Added Soccer animation  to the start screen- Keerthi

--- soccer_reversi.js
+++ soccer_reversi.js
@@ -1,19 +1,25 @@
 var sketchProc=function(processingInstance){ with (processingInstance){
 size(400, 400); 
 frameRate(60);
-/*title
 
-games 1
-games 2
-help/instructions
-about ( authors )
+// Use forces to move the ball
 
-character
-my board*/
-
-angleMode = "radians";
-var light;
+translate(0,0);
+angleMode = "radian";
+var oneDegree = 3.14/180;
+var centerX = 200;
+var centerY = 200;
 var keyArray = [];
+var myImages = [];
+var r=255;
+var g = 0;
+var b = 0;
+var initialized = 0;
+var keyPress = 0;
+var framerate = frameCount;
+
+// arvind
+var light;
 keyArray[UP] = keyArray[DOWN] = 0;
 var arrow_array = [];
 var coin_array = [];
@@ -24,6 +30,18 @@ var board_image = [];
 var drawing=0;
 var count=0;
 var ic=0;
+
+var game_vars1 = function(){		// declaring variable which controls state of game
+  this.startscreen = 1;
+  this.gamescreen1 = 0;
+  this.gamescreen2 = 0;
+  this.winscreen = 0;
+  this.helpscreen = 0;
+  this.aboutscreen=0;
+};
+
+/******* Reversi **********/
+
 for(i=0;i<8;i++){
     board_map[i]=[];
 }
@@ -132,14 +150,6 @@ board_map[4][3]=1;
 board_map[3][4]=1;
 
 
-var game_vars1 = function(){		// declaring variable which controls state of game
-  this.startscreen = 1;
-  this.gamescreen1 = 0;
-  this.gamescreen2 = 0;
-  this.winscreen = 0;
-  this.helpscreen = 0;
-  this.aboutscreen=0;
-};
 var game_vars = new game_vars1();
 
 var arrow =function(x,y,t){
@@ -354,14 +364,448 @@ coin_array.push( new coin(200,360,0));
 arrow_array.push( new arrow(100,110,1));
 arrow_array.push( new arrow(300,110,-1));
 
+
+/****** end reversi *************/
+
+
+
+var initializeSoccerCharacters = function(){
+    /***********************/
+    initialized  = 1;
+    background(40, 176, 19);
+    pushMatrix();
+    translate(100,100);
+    this.x = 0;
+    this.y = 0;
+    fill(38, 6, 6);
+    
+    ellipse(0, 0, 50,50); //head
+    fill(r,g,b);
+    
+    //body
+    quad(this.x-30, this.y+25, this.x+30, this.y+25, this.x+25, this.y+75, this.x-25, this.y+75);
+    
+    fill(235, 237, 208); // hand
+    ellipse(this.x-32,this.y+50, 10, 20);
+    ellipse(this.x+32,this.y+50, 10, 20);
+    
+    fill(r,g,b); 
+    rect(this.x-39,this.y+30, 15, 20, 10); // showlder
+    rect(this.x+25,this.y+30, 15, 20, 10); // showlder
+    
+    fill(255, 255, 255);
+    rect(this.x-25, this.y+75, 25, 20); //shorts
+    rect(this.x, this.y+75, 25, 20);
+    
+    fill(230, 229, 181);
+    rect(this.x-22, this.y+95, 20, 20);
+    rect(this.x+2, this.y+95, 20, 20);
+    
+    fill(0,0, 0);
+    rect(this.x-25, this.y+110, 23,15,50);
+    rect(this.x+2, this.y+110, 23,15,50);
+    fill(0, 21, 255,30);
+    myImages.push(get(100+this.x-37, 100+this.y-29, 76,150));
+    popMatrix();
+    
+    /***********End ***************/
+    /**********Start*************/
+    background(40, 176, 19);
+    pushMatrix();
+    scale(1);
+    translate(100,100);
+    this.x = 0;
+    this.y = 0;
+    fill(38, 6, 6);
+    
+    ellipse(0, 0, 50,50); //head
+    fill(r,g,b);
+    
+    //body
+    quad(this.x-30, this.y+25, this.x+30, this.y+25, this.x+25, this.y+75, this.x-25, this.y+75);
+    
+    fill(235, 237, 208); // hand
+    ellipse(this.x-32,this.y+50, 10, 20);
+    ellipse(this.x+32,this.y+50, 10, 20);
+    
+    fill(r,g,b); 
+    rect(this.x-39,this.y+30, 15, 20, 10); // showlder
+    rect(this.x+25,this.y+30, 15, 20, 10); // showlder
+    
+    
+    fill(255, 255, 255);
+    rect(this.x-25, this.y+75, 25, 20); //shorts
+    rect(this.x, this.y+75, 25, 20);
+    
+    //legs
+    fill(230, 229, 181);
+    //rect(this.x-22, this.y+95, 20, 20); // left leg
+    rect(this.x+2, this.y+95, 20, 20);
+    
+    fill(0,0, 0); 
+    rect(this.x-25, this.y+95, 23,15,50); // Changed 110 to 95 
+    rect(this.x+2, this.y+110, 23,15,50);
+    myImages.push(get(100+this.x-37, 100+this.y-29, 76,150));
+    popMatrix();
+    /**********************End**************/
+    /**********Start*************/
+    background(40, 176, 19);
+    pushMatrix();
+    scale(1);
+    translate(100,100);
+    this.x = 0;
+    this.y = 0;
+    fill(38, 6, 6);
+    
+    ellipse(0, 0, 50,50); //head
+    fill(r,g,b);
+    
+    //body
+    quad(this.x-30, this.y+25, this.x+30, this.y+25, this.x+25, this.y+75, this.x-25, this.y+75);
+    
+    fill(235, 237, 208); // hand
+    ellipse(this.x-32,this.y+50, 10, 20);
+    ellipse(this.x+32,this.y+50, 10, 20);
+    
+    fill(r,g,b); 
+    rect(this.x-39,this.y+30, 15, 20, 10); // showlder
+    rect(this.x+25,this.y+30, 15, 20, 10); // showlder
+    
+    
+    fill(255, 255, 255);
+    rect(this.x-25, this.y+75, 25, 20); //shorts
+    rect(this.x, this.y+75, 25, 20);
+    
+    //legs
+    fill(230, 229, 181);
+    rect(this.x-22, this.y+95, 20, 20); 
+    //rect(this.x+2, this.y+95, 20, 20);// right leg
+    
+    fill(0,0, 0); 
+    rect(this.x-25, this.y+105, 23,15,50);  
+    rect(this.x+2, this.y+95, 23,15,50);// Changed 110 to 95
+    myImages.push(get(100+this.x-37, 100+this.y-29, 76,150));
+    popMatrix();
+    /**********************End**************/
+    
+    /************ Front ******************/
+    /*************Start *****************/
+    /* Second Image */
+    background(40, 176, 19);
+    pushMatrix();
+    scale(1);
+    translate(100,100);
+    this.x = 0;
+    this.y = 0;
+    fill(229, 232, 207);
+    ellipse(this.x, this.y, 50,50); //head
+    
+    //eyes
+    fill(219, 107, 15);
+    ellipse(this.x-10, this.y+5, 5,7);
+    ellipse(this.x+10, this.y+5, 5,7);
+    arc(this.x,this.y+15,10,10,1*oneDegree, 180*oneDegree);
+    fill(74, 25, 25);
+    //hair
+    arc(this.x,this.y-5,50,40, 170*oneDegree,370*oneDegree);
+    fill(r,g,b);
+    
+    //body
+    quad(this.x-30, this.y+25, this.x+30, this.y+25, this.x+25, this.y+75, this.x-25, this.y+75);
+    
+    fill(235, 237, 208); // hand
+    ellipse(this.x-32,this.y+50, 10, 20);
+    ellipse(this.x+32,this.y+50, 10, 20);
+    
+    fill(r,g,b); 
+    rect(this.x-39,this.y+30, 15, 20, 10); // shoulder
+    rect(this.x+25,this.y+30, 15, 20, 10); // shoulder
+    
+    fill(255, 255, 255);
+    rect(this.x-25, this.y+75, 25, 20); //shorts
+    rect(this.x, this.y+75, 25, 20);
+    
+    fill(230, 229, 181);
+    rect(this.x-22, this.y+95, 20, 20);
+    rect(this.x+2, this.y+95, 20, 20);
+    
+    fill(0,0, 0);
+    rect(this.x-25, this.y+110, 23,15,50);
+    rect(this.x+2, this.y+110, 23,15,50);
+    myImages.push(get(100+this.x-37, 100+this.y-29, 76,150));
+    popMatrix();
+    /****************end*************************/
+    /*************Start *****************/
+    background(40, 176, 19);
+    /* Second Image */
+    pushMatrix();
+    scale(1);
+    translate(100,100);
+    this.x = 0;
+    this.y = 0;
+    fill(229, 232, 207);
+    ellipse(this.x, this.y, 50,50); //head
+    
+    //eyes
+    fill(219, 107, 15);
+    ellipse(this.x-10, this.y+5, 5,7);
+    ellipse(this.x+10, this.y+5, 5,7);
+    arc(this.x,this.y+15,10,10,1*oneDegree, 180*oneDegree);
+    fill(74, 25, 25);
+    //hair
+    arc(this.x,this.y-5,50,40, 170*oneDegree,370*oneDegree);
+    fill(r,g,b);
+    
+    //body
+    quad(this.x-30, this.y+25, this.x+30, this.y+25, this.x+25, this.y+75, this.x-25, this.y+75);
+    
+    fill(235, 237, 208); // hand
+    ellipse(this.x-32,this.y+50, 10, 20);
+    ellipse(this.x+32,this.y+50, 10, 20);
+    
+    fill(r,g,b); 
+    rect(this.x-39,this.y+30, 15, 20, 10); // shoulder
+    rect(this.x+25,this.y+30, 15, 20, 10); // shoulder
+    
+    fill(255, 255, 255);
+    rect(this.x-25, this.y+75, 25, 20); //shorts
+    rect(this.x, this.y+75, 25, 20);
+    
+    fill(230, 229, 181);
+    //rect(this.x-22, this.y+95, 20, 20); // left leg
+    rect(this.x+2, this.y+95, 20, 20);
+    
+    fill(0,0, 0);
+    rect(this.x-25, this.y+95, 23,15,50); // changed 110 t0 95
+    rect(this.x+2, this.y+110, 23,15,50);
+    myImages.push(get(100+this.x-37, 100+this.y-29, 76,150));
+    popMatrix();
+    /***********************end*********************/
+    
+    /*************Start *****************/
+    /* Second Image */
+    background(40, 176, 19);
+    pushMatrix();
+    scale(1);
+    translate(100,100);
+    this.x = 0;
+    this.y = 0;
+    fill(229, 232, 207);
+    ellipse(this.x, this.y, 50,50); //head
+    
+    //eyes
+    fill(219, 107, 15);
+    ellipse(this.x-10, this.y+5, 5,7);
+    ellipse(this.x+10, this.y+5, 5,7);
+    arc(this.x,this.y+15,10,10,1*oneDegree, 180*oneDegree);
+    fill(74, 25, 25);
+    //hair
+    arc(this.x,this.y-5,50,40, 170*oneDegree,370*oneDegree);
+    fill(r,g,b);
+    
+    //body
+    quad(this.x-30, this.y+25, this.x+30, this.y+25, this.x+25, this.y+75, this.x-25, this.y+75);
+    
+    fill(235, 237, 208); // hand
+    ellipse(this.x-32,this.y+50, 10, 20);
+    ellipse(this.x+32,this.y+50, 10, 20);
+    
+    fill(r,g,b); 
+    rect(this.x-39,this.y+30, 15, 20, 10); // shoulder
+    rect(this.x+25,this.y+30, 15, 20, 10); // shoulder
+    
+    fill(255, 255, 255);
+    rect(this.x-25, this.y+75, 25, 20); //shorts
+    rect(this.x, this.y+75, 25, 20);
+    
+    fill(230, 229, 181);
+    rect(this.x-22, this.y+95, 20, 20); 
+    //rect(this.x+2, this.y+95, 20, 20);// right leg
+    
+    fill(0,0, 0);
+    rect(this.x-25, this.y+105, 23,15,50); 
+    rect(this.x+2, this.y+95, 23,15,50);// changed 110 t0 95
+    myImages.push(get(100+this.x-37, 100+this.y-29, 76,150));
+    popMatrix();
+/***********************end*********************/
+};
+
+
+var displayCustomText = function(a, x, y, font){
+    var f = createFont("Bauhaus 93");
+    textFont(f, font);
+    text(a, x, y);
+};
+
+var drawBackground = function(){
+    noStroke();
+    fill(40, 176, 19);
+    rect(0, -200, 400, 800);
+    fill(34, 133, 16,100);
+    rect(0, -200, 30, 800);
+    for(var i=1; i<7; i++){
+        rect(60*i, -200, 30, 800);
+    }
+    stroke(255, 255, 255);
+    noFill();
+    strokeWeight(2);
+    rect(centerX-195,centerY-290, 390, 580);
+    strokeWeight(1);
+    line(0, centerY, 400, centerY);
+    ellipse(centerX, centerY, 100, 100);
+    
+    rect(centerX-100, centerY-290, 200, 100);
+    rect(centerX-50, centerY-290, 100, 50);
+    
+    rect(centerX - 100, centerY - 290, 200, 100);
+    rect(centerX - 50, centerY - 290, 100, 50);
+    
+    rect(centerX - 100, centerY - 290, 200, 100);
+    rect(centerX - 50, centerY - 290, 100, 50);
+    rect(centerX - 100, centerY + 290, 200, -100);
+    rect(centerX - 50, centerY + 290, 100, -50);
+    
+    arc(centerX, centerY-190, 50, 50, 0, oneDegree*180);
+    arc(centerX, centerY+190, 50, 50, oneDegree*180,oneDegree*360);
+    
+    rect(centerX-25, centerY-300, 50, 10);
+    rect(centerX-25, centerY+300, 50, -10);
+    // goal
+    
+};
+
+
+var keyPressed = function() {
+    keyArray[keyCode] = 1;
+    keyPress = 1;
+};
+var keyReleased = function() {
+    keyArray[keyCode] = 0;
+    keyPress = 0;
+};
+
+var ballObj = function(x, y){
+    this.position = new PVector(x,y);
+    this.size =40;
+    this.angle = 0;
+};var ball = new ballObj(300,300);
+
+ballObj.prototype.draw = function(x,y) {
+  pushMatrix();
+  translate(x,y);
+  rotate(this.angle);
+  fill(10, 196, 196,200);
+  ellipse(0, 0,this.size, this.size);
+  arc(0,0,this.size, this.size/2, 0, 180*oneDegree);
+  arc(0,-this.size/3,this.size, this.size/2, 20 * oneDegree, 160*oneDegree);
+  
+  arc(0,0,this.size/2,this.size,  90*oneDegree, 270*oneDegree);
+  arc(this.size/3,0,this.size/2,this.size, 120*oneDegree, 250*oneDegree);
+  fill(10, 196, 196,0);
+  
+  arc(0,0,this.size, this.size/2, 0, 180*oneDegree);
+  arc(0,-this.size/3,this.size, this.size/2,20 * oneDegree, 160*oneDegree);
+  fill(0, 0, 0);
+  displayCustomText("NIKE", -this.size/3.2,5, this.size/4, 30);
+  popMatrix();
+};
+
+
+var npcObj = function(x, y){
+    this.position = new PVector(x,y);
+    this.legs = 0;
+    this.face = 0;
+};var player = new npcObj(100, 100);
+
+npcObj.prototype.draw = function() {
+    if(this.face === 0){
+       image(myImages[this.legs], this.position.x, this.position.y, 15, 35);
+    }
+    else{
+       image(myImages[this.legs+3], this.position.x, this.position.y, 15, 35);
+    }
+    
+};
+npcObj.prototype.move = function() {
+    if(keyPress === 1){
+        if (keyArray[LEFT] === 1) {
+            this.position.x--;
+        }
+        if (keyArray[RIGHT] === 1) {
+            this.position.x++;
+        }
+        if (keyArray[UP] === 1) {
+            this.position.y--;
+            this.face = 0;
+        }
+        if (keyArray[DOWN] === 1) {
+            this.position.y++;
+            this.face = 1;
+        }  
+        if(framerate < frameCount-5){
+            framerate = frameCount;
+            this. legs = (this.legs+1)%3;
+        }
+    }
+    var npcCenter = new PVector(15,20);
+    npcCenter =  PVector.add(npcCenter, this.position);
+    var vec = PVector.sub(npcCenter, ball.position);
+    if(vec.mag() < 10){
+        if (keyArray[LEFT] === 1) {
+            ball.position.x-=5;
+        }
+        if (keyArray[RIGHT] === 1) {
+            ball.position.x+=5;
+        }
+        if (keyArray[UP] === 1) {
+            ball.position.y-=5;
+        }
+        if (keyArray[DOWN] === 1) {
+            ball.position.y+=5;
+        }  
+    }
+};
+
+
+var frame_Start = frameCount;
+var initialScale = 1;
+var initialPlayer = new npcObj(-5, 100);
+var startScreenAnimation = function(){
+    pushMatrix();
+    initialPlayer.face= 1;
+    pushMatrix();
+    scale(initialScale);
+    translate(initialScale*5,0);
+    initialPlayer.draw();
+    if(frame_Start < frameCount-10){
+        initialScale = initialScale+0.1;
+        frame_Start  = frameCount;
+         initialPlayer.legs = (initialPlayer.legs+1)%3;
+    }
+    popMatrix();
+    
+    ball.draw(ball.position.x, ball.position.y);
+    
+    if(initialScale>4){
+        initialPlayer.position.y = 100;
+        initialScale = 1;
+    }    
+};
+
 draw = function() {
     
-    this.width=400;
-    this.height=400;
-    if(game_vars.startscreen === 1){
+     if(game_vars.startscreen === 1){
         pushMatrix();
         //translate(300,300);
-        background(19, 150, 22);
+        background(40, 176, 19);
+        
+        /* Soccer */
+        if(initialized === 0){
+            initializeSoccerCharacters();
+        }
+        startScreenAnimation();
+        /* Soccer End */
+        
         board.draw();
         arrow_array[0].draw();
         arrow_array[1].draw();
@@ -376,33 +820,15 @@ draw = function() {
         text("About " ,160,220,300,200);
         popMatrix();
     }
-    if(game_vars.helpscreen === 1){
-        background(12, 125, 14);
-        pushMatrix();
-        if(frameCount - count > 90){
-            count = frameCount;
-            ic++;
-        }
-        if(ic===3){ic=0;}
-        image(board_image[ic],270,30,120,120);
-        translate(0,-200);
-        translate(0,-50);
-        stroke(0, 0, 0);
-        light.draw();
-        popMatrix();
-
-
-        fill(4, 0, 255);
-        textSize(30);
-        text("HELP",30,20,300,200);
-        textSize(15);
-        text("Reversi is a two player board game. Each player controls either the black or the white tile.  The black gets to move first. At any point in the game any square on the 8x8 board can only be occupied by either a tile of one color.",30,50,220,400);
-        text("Each turn consists of one player placing one tile of his color in any one of the spaces which allows him to capture his opponent’s tiles. The tiles can be captured vertically, horizontally and diagonally. The above figures show all the legal positions available and how the captured pieces are flipped. The game is played until no position on the board is a legal position for either of the player. If one player at any point in the game does not have a legal move, he must pass. The winner is the player who has the most number of tiles at the end of the game.",30,190,320,400);
-        //text("It is a ",30,70,300,200);
-        
-
-    }
+     
+     //ball.draw(ball.position.x, ball.position.y);
+    /* player.draw();
+     
+     player.move();*/ 
+     
+     
      
 };
+
 
 }};
